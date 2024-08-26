df227d7fed35e5d0861b117f25dd676696941868
AdityaShuk1a <adityasarika0292004@gmail.com>
2024-08-26 20:37:48 +0530

    Third problem done

---
+++ Daily_Problems/Day1/Third_problem.cpp
@@ -0,0 +1,18 @@
+/*
+Write a C++ program that takes an integer as input from the user and outputs its square.
+
+Sample Input:- Enter a number: 7
+
+Sample Output :- The square of 7 is: 49
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main (){
+    int a;
+    cin >>a;
+    cout << "Square of number is: " << a*a;
+
+}
