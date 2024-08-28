1479635e3daaafac0be1b7967348d665bf7659a7
AdityaShuk1a <adityasarika0292004@gmail.com>
2024-08-28 20:31:51 +0530

    day5 1st problem done

--- Daily_Problems/Day4/Day4_2nd_problem.cpp
+++ Daily_Problems/Day4/Day4_2nd_problem.cpp
@@ -25,7 +25,7 @@ int main() {
         if (a[i]=='+' || a[i]=='-'){
             i1 = (int)a[i+1] -48 ;
         }
-
+ 
     }
     for (int i=0; i<b.size(); i++){
         if (b[i]=='+' || b[i]=='-'){
