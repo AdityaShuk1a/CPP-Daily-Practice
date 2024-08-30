1d9bee6dbde65ed188cd1ebc713fae6e0a529dd6
AdityaShuk1a <adityasarika0292004@gmail.com>
2024-08-30 19:26:48 +0530

    practicing vector questions

--- Daily_Problems/Day8/Day8_practice_problem.cpp
+++ Daily_Problems/Day8/Day8_practice_problem.cpp
@@ -7,7 +7,7 @@ arr[] = {4, 1, 3, 9, 7}
 
 Output:
 1 3 4 7 9
-
+    
 */
 
 #include <bits/stdc++.h>
---
+++ Daily_Problems/Day9/first_class.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+// #include <vector>
+using namespace std;
+int main (){
+    int a;
+    vector <string> n(10, "0");
+    n.at(5) = "10";
+
+    for (string i:n){
+        cout << i<<endl;
+    }
+    
+    
+
+
+    return 0;
+}
