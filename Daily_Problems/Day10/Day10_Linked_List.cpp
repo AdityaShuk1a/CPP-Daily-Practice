31a77a0552eddbe85b92d6996484ab5dfd0ac630
AdityaShuk1a <adityasarika0292004@gmail.com>
2024-08-31 15:50:00 +0530

    Started linked list

---
+++ Daily_Problems/Day10/Day10_Linked_List.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+/*
+linked list is linear data structure 
+blocks idhar udhar pade rehte ha
+they are connected by links by pointers
+each block points some other block which then become a chain 
+to reach a particular block we have to go throw multiple blocks
+first node is head node which is very important 
+head gaya sab gaye
+we have to create the head node
+singly linked list is the which point the other node but we cant reverse the process kyunki uske pass sabka address hai baaki ke pass nhi ha
+we need data and a pointer 
+data hold data value pointer holds the address
+we need node pointer
+in the node class we need 2 things data and a node ponter (Node *next)
+last node points at null address
+
+*/
+
+int main(){
+
+    return 0;
+}
