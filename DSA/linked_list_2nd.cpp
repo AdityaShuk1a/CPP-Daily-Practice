6a6f897f6ea5f432004fd57d9130e8b4b7eb7f9b
AdityaShuk1a <adityasarika0292004@gmail.com>
2024-09-06 23:34:53 +0530

    single linked list concept clear

--- DSA/linked_list_2nd.cpp
+++ DSA/linked_list_2nd.cpp
@@ -1,21 +1,105 @@
+// #include <bits/stdc++.h>
+
+// using namespace std;
+
+// class Node{
+//     public:
+//         int data;
+//         Node* next;
+//         Node (int val){
+//             this->data = val;
+//             next = NULL;
+//         }
+// };  
+
+// void inserttail(Node* &head, int val){
+//     Node* n = new Node(val);
+//     if(head == NULL){
+//         head = n;
+//         return;
+//     }
+//     Node* temp = head;
+//     while (temp->next!=NULL){
+//         temp = temp->next;
+//     }
+//     temp->next = n;
+
+// }
+// void printData(Node* &head){
+//     Node* temp = head;
+//     while (temp->next!=NULL){
+//         cout << temp->data << "->";
+//         temp = temp->next;
+//     }
+//     cout <<  "BLABA" << endl;
+// }
+
+// // void delete(Node* &head, int postition){
+// //     Node* temp = head;
+// //     while (temp->next== postition-1){
+// //         temp->next = temp->next->next;
+// //         return;
+// //     }
+// // }
+
+
+// int main (){
+//     Node* head = NULL;
+//     inserttail(head, 1);
+//     inserttail(head, 2);
+//     inserttail(head, 3);
+//     inserttail(head, 4);
+//     inserttail(head, 5);
+//     inserttail(head, 6);
+//     // delete(head, 1);
+//     printData(head);
+// }
+
 #include <bits/stdc++.h>
 
 using namespace std;
+
+// struct account {
+//     int data;
+//     string s;
+//     bool ss;
+//     char valu;
+// };
+
+// union data {
+//     account aa;
+//     data() { new(&aa) account(); } // Placement new to construct the account
+//     ~data() { aa.~account(); }     // Explicitly call the destructor
+// };
+
+// int main() {
+//     data a;
+//     a.aa.data = 10;
+//     a.aa.s = "haa";
+//     a.aa.ss = false;
+//     a.aa.valu = 'a';
+//     cout << a.aa.data << " " << a.aa.valu << " " << a.aa.s << " " << a.aa.ss << endl;
+
+//     return 0;
+// }
+
 
 class Node{
     public:
         int data;
         Node* next;
-        Node (int val){
+        Node(int val){
             this->data = val;
             next = NULL;
         }
-};  
+};
 
-void inserttail(Node* &head, int val){
+
+void insert(Node* &head, int val){
     Node* n = new Node(val);
     if(head == NULL){
         head = n;
+        // next = NULL;
         return;
     }
     Node* temp = head;
@@ -23,34 +107,23 @@ void inserttail(Node* &head, int val){
         temp = temp->next;
     }
     temp->next = n;
-
 }
-void printData(Node* &head){
+
+void print(Node* &head){
     Node* temp = head;
-    while (temp->next!=NULL){
+    while (temp!= NULL){
         cout << temp->data << "->";
-        temp = temp->next;
+        temp = temp->next; 
     }
-    cout <<  "BLABA" << endl;
+    cout << "NULL";
+    
+
 }
-
-// void delete(Node* &head, int postition){
-//     Node* temp = head;
-//     while (temp->next== postition-1){
-//         temp->next = temp->next->next;
-//         return;
-//     }
-// }
-
 
 int main (){
     Node* head = NULL;
-    inserttail(head, 1);
-    inserttail(head, 2);
-    inserttail(head, 3);
-    inserttail(head, 4);
-    inserttail(head, 5);
-    inserttail(head, 6);
-    // delete(head, 1);
-    printData(head);
+    insert(head, 1);
+    insert(head, 3);
+    insert(head, 4);
+    print(head);
 }
