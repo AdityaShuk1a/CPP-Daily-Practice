9bfb4b0384d3a24b2ea39bdb3ae4711b7715f693
AdityaShuk1a <adityasarika0292004@gmail.com>
2024-09-15 17:29:40 +0530

    pacticing linked list DLL and SLL

--- DSA/linked_list_1st_problem.cpp
+++ DSA/linked_list_1st_problem.cpp
@@ -11,6 +11,15 @@ class Node{
             next = NULL;
 
         }
+        ~Node(){
+            int value = this->data;
+            if(this->next!=NULL){
+                delete next;
+                this->next = NULL;
+
+            }
+            cout << " your memory for the particular node is freed enjoy have a nice dday." << endl;
+        }
 };
 
 void insertAtHead(Node* &head, int val){
@@ -21,22 +30,25 @@ void insertAtHead(Node* &head, int val){
     head = n;
 }
 
-void insertAtTail(Node* &head, int val){
+void insertAtTail(Node* &head,Node* &tail, int val){
     //create a new node in which data feild will have val and next pointer will be NULL
     Node* n = new Node(val);
-
+ 
     if(head == NULL){
         head = n;
+        tail = head;
         return;
     }
 
-    Node* temp = head;
+    // Node* temp = head;
 
-    while(temp->next != NULL){
-        temp = temp->next;
-    }
+    tail->next = n;
+    tail = tail->next;
+    // while(temp->next != NULL){
+    //     temp = temp->next;
+    // }
 
-    temp->next = n;
+    // temp->next = n;
 
 
 }
@@ -53,17 +65,36 @@ void display(Node* head){
 
     
 }
+void deleteN(Node* &head, int position){
+    Node * temp = head;
+    if(position == 1){
+        Node* a = head;
+        head = head->next;
+        a->next = NULL;
+        delete a;
+        return;
+    }
+    for (int i=0; i<position-1; i++){
+        temp = temp->next;
+
+    }
+    Node * a = temp->next;
+    temp->next = temp->next->next;
+    a->next = NULL;
+    delete(a);
+}
 
 int main (){
 
     Node* head = NULL;
-    insertAtTail(head, 1);
-    insertAtTail(head, 2);
-    insertAtTail(head, 3);
-    insertAtTail(head, 4);
+    Node* tail = head;
+    insertAtTail(head,tail, 1);
+    insertAtTail(head,tail, 2);
+    insertAtTail(head,tail, 3);
+    insertAtTail(head,tail, 4);
 
-    insertAtHead(head, 7);
-    
+    // insertAtHead(head, 7);
+    deleteN(head, 1);
 
     display(head);
     
